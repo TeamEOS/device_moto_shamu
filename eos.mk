b62fdd89568f72c53d356422a965c203d3a7fe27
Randall Rushing <randall.rushing@gmail.com>
2015-10-17 15:33:00 -0700

    Bring in eos make and deps and update fingerprint

---
+++ eos.mk
@@ -0,0 +1,24 @@
+# Inherit some common Eos stuff.
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
+
+# Enhanced NFC
+#$(call inherit-product, vendor/eos/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+# Copy Bootanimation
+PRODUCT_COPY_FILES += \
+vendor/eos/prebuilt/common/bootanimation/1080.zip:system/media/bootanimation.zip
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := eos_shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
+
+TARGET_VENDOR := moto
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:6.0/MRA58K/2256973:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 6.0 MRA58K 2256973 release-keys"
